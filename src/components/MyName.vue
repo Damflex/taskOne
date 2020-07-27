595b7db9deb8ecb865d6cfe420d9e9860de1d4d7
uchedotphp <uchedotphp@gmail.com>
2020-07-27 06:59:21 +0100

    I commit this task unto the name of Jesus Christ. amen

---
+++ src/components/MyName.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="center">
+    <p>{{myName}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyNameComponent",
+  props: {
+    myName: {
+      type: String,
+      default: "Nwulu Uchechukwu",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+}
+p {
+  color: red;
+}
+</style>
